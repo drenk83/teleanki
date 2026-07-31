46882c1d12c6ce471c704d64da32eb242952a8ca
drenk83 <ytdrenk@gmail.com>
2026-07-31 20:56:39 +0300

    first tg bot code

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log/slog"
 	"os"
+	"os/signal"
 
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := godotenv.Load(); err != nil {
+		slog.Info(".env file not found")
 	}
 
 	tgToken := os.Getenv("TG_TOKEN")
-	fmt.Println(tgToken)
+	if tgToken == "" {
+		slog.Error("TG_TOKEN is not set")
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{}
+	b, err := bot.New(tgToken, opts...)
+	if err != nil {
+		slog.Error("Failed to create bot", "error", err)
+		os.Exit(1)
+	}
+
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeCommand, handler)
+
+	slog.Info("Bot is starting...")
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	if update.Message == nil {
+		return
+	}
+
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "Hello",
+	})
+
+	if err != nil {
+		slog.Error("Failed to send message", "error", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/drenk83/teleanki
 
 go 1.26.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-telegram/bot v1.22.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
